7cf971b238f54e57590c2b939e309ebfaf94aa8e
Mário Aragão <marioestudos.nuvem@gmail.com>
2025-11-10 14:29:06 -0300

    add fuctions, insert in internal node, some changes in insert in leaf

--- b-tree/BPlusTree.py
+++ b-tree/BPlusTree.py
@@ -21,32 +21,38 @@ class BPlusTree:
             leaf.values.insert(i, value)
         return leaf
 
-    def split_leaf(self, leaf: Leaf):
-        # Criação do novo nó folha
-        new_leaf = Leaf(self.order)
+    def split_intenal(self, node: Internal, parent_stack):
+        pass
 
-        # Meio do nó
+    def insert_in_internal(self, internal: Internal, leaf: Leaf, parent_stack):
+        i = 0
+        new_key = leaf.keys[0]
+        while i < internal.keys_size and new_key > internal.get_key_by_index(i):
+            i += 1
+        internal.keys.insert(i, new_key)
+        internal.nodes.insert(i + 1, leaf)
+        if internal.is_full:
+            self.split_intenal(internal, parent_stack)
+
+    def split_leaf(self, leaf: Leaf, parent_stack=[]):
+        new_leaf = Leaf(self.order)
         mid = math.ceil(leaf.keys_size / 2)
 
-        # Popula o novo nó folha com os valores de key e value
         new_leaf.keys = leaf.keys[mid:]
         new_leaf.values = leaf.values[mid:]
 
-        # Atualiza os valores da folha que estava cheia
         leaf.keys = leaf.keys[:mid]
         leaf.values = leaf.values[:mid]
 
-        # Cria o no interno
-        internal = Internal(self.order)
-        # Adiciona a chave do meio do nó que estava cheio para ser a nova chave do nó interno
-        internal.keys = [new_leaf.keys[0]]
-
-        # Adição do nos folhas na lista de filhos de no interno
-        internal.nodes = [leaf, new_leaf]
-
-        leaf.next = new_leaf
-
-        return internal
+        # Folha é root
+        if not parent_stack:
+            internal = Internal(self.order)
+            internal.keys = [new_leaf.keys[0]]
+            internal.nodes = [leaf, new_leaf]
+            leaf.next = new_leaf
+            return
+        parent = parent_stack.pop()
+        self.insert_in_internal(parent, new_leaf, parent_stack)
 
     def insert(self, key, value):
         # Percorrer a arvore procurando por onde o nó deve entrar
@@ -60,17 +66,21 @@ class BPlusTree:
             leaf = self.insert_in_leaf(root, key, value)
             # Folha cheia
             if leaf.is_full:
-                self.root = self.split_leaf(leaf)  # Root é nó interno
+                self.root = self.split_leaf(leaf)
         else:
-            parent_stack = []
+            # Caso em que o root é um nó interno
+            parent_stack = []  # Caminho do nó até a folha
             node = root
             while isinstance(node, Internal):
                 parent_stack.append(node)
                 i = 0
-                while i < node.keys_size and key >= node.get_key_by_index(i):
+                while i < node.keys_size and key > node.get_key_by_index(i):
                     i += 1
                 node = node.nodes[i]
             leaf = node
+            self.insert_in_leaf(leaf, key, value)
+            if leaf.is_full:
+                self.split_leaf(leaf, parent_stack)
 
         # def search(self, key):
         """Retorna o valor associado à chave, se existir."""
@@ -83,3 +93,13 @@ class BPlusTree:
         # def display(self):
         """Exibe a estrutura da árvore (nós internos e folhas)."""
         # pass
+
+
+key = [30]
+nodes = ["jp", "ma"]
+
+key.insert(0, 15)
+nodes.insert(1, "gg")
+
+print(key)
+print(nodes)
