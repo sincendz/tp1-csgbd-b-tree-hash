7131a5f1e09d06881c91dac897dff056d74def2c
Mário Aragão <marioestudos.nuvem@gmail.com>
2025-11-11 08:55:51 -0300

    main testes

--- b-tree/main.py
+++ b-tree/main.py
@@ -6,19 +6,33 @@ def main():
 
     # Insere algumas chaves
     a = [
-        (20, "A"), (10, "B"), (30, "C"), (40, "D"),
-        (50, "E"), (60, "F"), (35, "G"), (25, "H"),
-        (15, "E"), (5, "F"), (45, "G"), (55, "H"),
-        (70, "E"), (80, "F"), (75, "G"), (85, "H"),
-        (90, "E"), (95, "F")
+        (20, "A"),
+        (10, "B"),
+        (30, "C"),
+        (40, "D"),
+        (50, "E"),
+        (60, "F"),
+        (35, "G"),
+        (25, "H"),
+        (15, "E"),
+        (5, "F"),
+        (45, "G"),
+        (55, "H"),
+        (70, "E"),
+        (80, "Mario"),
+        (75, "G"),
+        (85, "H"),
+        (90, "E"),
+        (95, "Fruta"),
     ]
 
-    #a = [20,10,30,40,50,60,35,3,25,15,45]
-    #for k, v in dados:
-    for i, _ in a:
-        #i = input("Enter a number: ")
-        bpt.insert(i, "a")
-        bpt.display()
-
+    # a = [20,10,30,40,50,60,35,3,25,15,45]
+    # for k, v in dados:
+    for i,v in a:
+        # i = input("Enter a number: ")
+        bpt.insert(i, v)
+    
+    #bpt.to_graphviz("gurgel")
+    print(bpt.search(96))
 if __name__ == "__main__":
     main()
