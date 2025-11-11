80373e34ed1404844f84ea2de489263666caca0f
Mário Aragão <marioestudos.nuvem@gmail.com>
2025-11-11 15:29:47 -0300

    a

--- b-tree/BPlusTree.py
+++ b-tree/BPlusTree.py
@@ -173,7 +173,7 @@ class BPlusTree:
             key_index = node.keys.index(key)
             node.keys.pop(key_index)
             node.values.pop(key_index)
-            # Verifica se ainda tem elementos na folha
+
             new_internal_velue = -1
             if node.keys_size > 0:
                 new_internal_velue = node.get_key_by_index(0)
