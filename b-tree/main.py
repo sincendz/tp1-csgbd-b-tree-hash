6ff1bb3a7707f7b434b2cd2566c8d9d8f2118568
Mário Aragão <marioestudos.nuvem@gmail.com>
2025-11-14 16:17:01 -0300

    main para testes

--- b-tree/main.py
+++ b-tree/main.py
@@ -1,33 +1,43 @@
 from BPlusTree import BPlusTree
+import random
 
 def main():
     # Cria uma árvore de ordem 4 (por exemplo)
     bpt = BPlusTree(order=4)
 
-    a = [0,1,2,3,60,70,5,6,7]
+    #Caso a professora queira inserir valores aletorios,
+    # eh so remover o comentario
+    #a = [i for i in range(15)]
+    #random.shuffle(a)
     # a = [1,2,3,4]
-    for ele in a:
-        bpt.insert(int(ele),'')
+    # for ele in a:
+    #     bpt.insert(int(ele),'')
 
-    bpt.display()
-    # bpt.insert(62,'')
-    # bpt.display()
-    # return
+    #Inserção dos valores
+    print("Inserção de valores")
+    while True:
+        a = input("Digite um valor: ")
+        if a == 'end':
+            break
+        bpt.insert(int(a),'')
+        bpt.display()
 
-    # print("Inserção de valores")
-    # while True:
-    #     a = input("Digite um valor: ")
-    #     if a == 'end':
-    #         break
-    #     bpt.insert(int(a),'')
-    #     bpt.display()
-
+    #Busca dos valores
     print("Busca por valores")
     while True:
         a = input("Digite um valor buscar: ")
         if a == 'end':
             break
         print(bpt.search(int(a)))
+
+    #Remoção
+    print("Remover valores")
+    while True:
+        a = input("Digite um valor remover: ")
+        if a == 'end':
+            break
+        bpt.remove(int(a))
+        bpt.display()
 
 if __name__ == "__main__":
     main()
