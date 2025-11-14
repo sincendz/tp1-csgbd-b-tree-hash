6658f43ef2eb1674ad2141228b1144ba46059be6
Mário Aragão <marioestudos.nuvem@gmail.com>
2025-11-14 14:44:38 -0300

    main testes

--- b-tree/main.py
+++ b-tree/main.py
@@ -4,21 +4,30 @@ def main():
     # Cria uma árvore de ordem 4 (por exemplo)
     bpt = BPlusTree(order=4)
 
-    # a = [0,1,2,3,60,70,5,6,7]
-    a = [1,2,3,4]
+    a = [0,1,2,3,60,70,5,6,7]
+    # a = [1,2,3,4]
     for ele in a:
-        bpt.insert(ele,'')
+        bpt.insert(int(ele),'')
 
     bpt.display()
     # bpt.insert(62,'')
     # bpt.display()
     # return
 
+    # print("Inserção de valores")
+    # while True:
+    #     a = input("Digite um valor: ")
+    #     if a == 'end':
+    #         break
+    #     bpt.insert(int(a),'')
+    #     bpt.display()
+
+    print("Busca por valores")
     while True:
-        a = input()
+        a = input("Digite um valor buscar: ")
         if a == 'end':
             break
-        bpt.insert(int(a),'')
-        bpt.display()
+        print(bpt.search(int(a)))
+
 if __name__ == "__main__":
     main()
